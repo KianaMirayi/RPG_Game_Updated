e36de561a920d4e37c68ae0fbf5c16a7a14067e6
KianaMirayi <1916599843@qq.com>
2024-07-19 02:56:50 +0800

    Add files via upload

---
+++ UI/UI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchTo(GameObject _menu)  //切换UI界面的不同菜单: 人物、技能、工坊、选项
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);  //遍历每一个该组件下的自组件
+        }
+
+        if (_menu != null)
+        { 
+            _menu.SetActive(true);  //当鼠标点击特定菜单后将激活该菜单
+        }
+    }
+
+}
