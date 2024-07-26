42ad49b6444b9f850ccad9012188be0bdf553908
KianaMirayi <1916599843@qq.com>
2024-07-27 03:41:08 +0800

    Add files via upload

--- Save&Load/GameData.cs
+++ Save&Load/GameData.cs
@@ -15,8 +15,18 @@ public class GameData
     public SerializableDictionary<string, bool> checkPoints;
     public string closetCheckPointId;
 
+    public float lostCurrencyX;
+    public float lostCurrencyY;
+    public int lostCurrencyAmount;
+
+    public SerializableDictionary<string, float> volumeSettings;
+
     public GameData()
     { 
+        this.lostCurrencyX = 0;
+        this.lostCurrencyY = 0;
+        this.lostCurrencyAmount = 0;
+
         this.currency = 0; // 当开始新游戏时，游戏初始货币为0 
 
         inventory = new SerializableDictionary<string, int>(); //初始化记录装备的字典
@@ -27,7 +37,7 @@ public class GameData
         checkPoints = new SerializableDictionary<string, bool>();
 
 
-
+        volumeSettings = new SerializableDictionary<string, float>();
 
 
     }
